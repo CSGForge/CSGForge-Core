d99a4e12bb4ed591aca16393f0f430ba0e84c338
JarrodDoyle <thejarroddoyle@gmail.com>
2022-02-03 19:22:11 +0000

    Plane transformation added

--- src/Plane.cpp
+++ src/Plane.cpp
@@ -2,4 +2,10 @@
 
 namespace ForgeCore
 {
+    void Plane::Transform(glm::mat4 transformation) {
+        glm::vec4 plane{mNormal, mOffset};
+        plane = glm::transpose(glm::inverse(transformation)) * plane;
+        mNormal = glm::vec3{plane.x, plane.y, plane.z};
+        mOffset = plane.w;
+    }
 }
--- src/Plane.hpp
+++ src/Plane.hpp
@@ -12,5 +12,7 @@ namespace ForgeCore
 
         glm::vec3 mNormal;
         float mOffset;
+
+        void Transform(glm::mat4 transformation);
     };
 }
